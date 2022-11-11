cc53d7ed454139d2073ee1d2982076c3cda8d56e
Senthilsagayaraj <senthil.sagayaraj@gmail.com>
2022-11-12 00:43:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const app = express();
+const bodyparser=require('body-parser');
+
+app.use(express.json());
+
+app.get('/', (req,res)=>{
+	res.send('Hello World');
+});
+
+	app.get('/sumpvolume', (req,res)=>{
+	let n1 = 4;
+	let n2 = 10;
+	let sum = n1+n2;
+	res.send("sum is"+sum);
+});
+
+app.post('/sumpvolume', (req,res)=>{
+		
+	let volume=0;
+	let length = req.body.length;
+	let breadth = req.body.breadth; 
+	let height = req.body.height;
+	volume = ((length*breadth*height)*29.5);
+	res.send('sump volume is '+volume+' litres');
+});
+
+app.listen(3000,(res)=>{
+	console.log('server started at port 3000')
+})
+
